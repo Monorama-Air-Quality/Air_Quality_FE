ddba1fc023e2c298ed4bcd9fbbe6c809554311aa
sssungjin <dkdlwls576@gmail.com>
2024-11-05 17:33:17 +0900

    feat: 웹소켓 API 프록시 설정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,23 +1,22 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
+  // REST API 프록시
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080/api",
+      target: "http://localhost:8080",
       changeOrigin: true,
-      secure: false,
-      logLevel: "debug",
-      onProxyRes: function (proxyRes, req, res) {
-        proxyRes.headers["Access-Control-Allow-Origin"] = "*";
-      },
-      onError: function (err, req, res) {
-        console.log("Proxy Error:", err);
-        res.writeHead(500, {
-          "Content-Type": "text/plain",
-        });
-        res.end("Something went wrong with the proxy.");
-      },
+    })
+  );
+
+  // WebSocket 프록시
+  app.use(
+    '/ws',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',
+      ws: true,
+      changeOrigin: true
     })
   );
 };
